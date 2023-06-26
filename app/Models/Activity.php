a73a67608a255b913ca32a7eda76e95d426090e8
Ivan Rizkyanto <ivanrizqy.ir@gmail.com>
2023-06-26 10:08:11 +0700

    Create activity table

---
+++ app/Models/Activity.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use App\Models\User;
+
+class Activity extends Model
+{
+    use HasFactory, HasUuids;
+
+    /**
+     * Get the User that owns the Activity
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
