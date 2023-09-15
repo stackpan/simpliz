afc6ef4b2a7e8a8024713bcf5e80779fdc23101a
Ivan <ivanrizqy.ir@gmail.com>
2023-09-15 13:11:32 +0700

    Fix confirm password view

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
+    <div class="mb-4 text-sm text-primary-content">
         {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
     </div>
 
@@ -7,21 +7,17 @@
         @csrf
 
         <!-- Password -->
-        <div>
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        <div class="form-control">
+            <x-input.label for="password" :text="__('Password')"/>
+            <x-input.text id="password" class="mt-2"
+                          type="password"
+                          name="password"
+                          required autocomplete="current-password"/>
+            <x-input.error :messages="$errors->get('password')" class="mt-2"/>
         </div>
 
-        <div class="flex justify-end mt-4">
-            <x-primary-button>
-                {{ __('Confirm') }}
-            </x-primary-button>
+        <div class="flex items-center justify-end mt-8">
+            <x-button.primary>{{ __('Confirm') }}</x-button.primary>
         </div>
     </form>
 </x-guest-layout>
