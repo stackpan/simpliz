5535cbd9d6ad5b2973f381950e5bb683e11ed36c
Ivan Rizkyanto <ivanrizqy.ir@gmail.com>
2024-02-25 13:47:37 +0700

    Fix search participants response

--- app/Http/Controllers/Api/SearchParticipantController.php
+++ app/Http/Controllers/Api/SearchParticipantController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\UserCollection;
+use App\Http\Resources\UserParticipantCollection;
 use App\Services\ParticipantService;
 use Illuminate\Http\Request;
 
@@ -18,12 +18,15 @@ class SearchParticipantController extends Controller
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request): UserCollection
+    public function __invoke(Request $request): UserParticipantCollection
     {
         $search = $request->query('search', '');
         $limit = $request->query('limit', 10);
 
         $results = $this->participantService->search($search, $limit);
-        return new UserCollection($results);
+        return (new UserParticipantCollection($results))
+            ->additional([
+                'message' => __('message.success'),
+            ]);
     }
 }
